90a46d5545359f6fb12788e45f6a9570c5986afc
ViltvodleVI <antonio.ciociola@outlook.it>
2018-04-26 21:53:11 +0200

    Initial commit

---
+++ android/src/com/artoolkitintegration/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.artoolkitintegration.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.artoolkitintegration.game.ArtoolkitIntegration;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ArtoolkitIntegration(), config);
+	}
+}
